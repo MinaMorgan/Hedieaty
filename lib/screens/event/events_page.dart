7aa9e70cbd8347f42c513f09a1e9f81ec6da4957
Mina Morgan <minammorgan21@gmail.com>
2024-11-01 17:21:19 +0200

    Add Event Button added

--- lib/screens/event/events_page.dart
+++ lib/screens/event/events_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gradient_app_bar/flutter_gradient_app_bar.dart';
+
+import '/screens/event/add_event_page.dart';
 import '/widgets/custom_bottom_navigation_bar.dart';
 
 class EventsPage extends StatefulWidget {
@@ -12,28 +15,49 @@ class _EventsPageState extends State<EventsPage> {
   // Dummy data for the events list
   final List<Map<String, String>> events = [
     {
-      'title': 'Rotaract Club Meeting',
-      'description': 'Join us for our weekly club meeting to discuss upcoming projects and events.',
-      'date': '2024-10-25',
+      'title': 'My Birthday Celebration',
+      'description':
+          'Celebrating my birthday with close friends and family. I would love thoughtful gifts like books, tech gadgets, or even handmade items.',
+      'date': '2024-10-30',
     },
     {
-      'title': 'Charity Fun Run',
-      'description': 'Participate in our annual charity fun run to raise funds for local charities.',
-      'date': '2024-11-10',
+      'title': 'Graduation Party',
+      'description':
+          'It’s finally graduation day! If you’re thinking of bringing a gift, I’d appreciate anything memorable, like photo albums, personalized items, or even something for my new apartment.',
+      'date': '2025-01-15',
     },
     {
-      'title': 'Tree Planting Event',
-      'description': 'Help us make the world greener by planting trees in the community park.',
-      'date': '2024-12-05',
-    },
+      'title': 'Engagement Celebration',
+      'description':
+          'Join us in celebrating my engagement! If you’re considering a gift, something for our new life together like dinnerware or bedding would be perfect.',
+      'date': '2025-03-10',
+    }
   ];
+
+  void _addNewEvent() {
+    // Navigate to the AddEventPage to add a new event
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => AddEventPage()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Upcoming Events'),
-        backgroundColor: Color(0xFF1E3A8A),
+      appBar: GradientAppBar(
+        title: const Text('Events'),
+        gradient: const LinearGradient(
+          colors: [Color(0xFF1E3A8A), Color(0xFF1E88E5)],
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              _addNewEvent();
+            },
+          ),
+        ],
       ),
       body: ListView.builder(
         padding: const EdgeInsets.all(8.0),
@@ -65,7 +89,8 @@ class _EventsPageState extends State<EventsPage> {
                   context: context,
                   builder: (context) => AlertDialog(
                     title: Text(event['title']!),
-                    content: Text('More details about "${event['title']}" event.'),
+                    content:
+                        Text('More details about "${event['title']}" event.'),
                     actions: [
                       TextButton(
                         onPressed: () => Navigator.pop(context),
@@ -79,7 +104,7 @@ class _EventsPageState extends State<EventsPage> {
           );
         },
       ),
-      bottomNavigationBar: CustomBottomNavigationBar(
+      bottomNavigationBar: const CustomBottomNavigationBar(
         currentIndex: 1, // Set to 1 to highlight the "Events" tab
       ),
     );
