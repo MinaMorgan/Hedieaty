612644940673271633616d79efcdea227db4e6bc
Mina Morgan <minammorgan21@gmail.com>
2024-11-25 03:02:12 +0200

    Local Database Edited

--- lib/services/db_manager.dart
+++ lib/services/db_manager.dart
@@ -1,7 +1,6 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:encrypt/encrypt.dart' as encrypt;
 
 class DatabaseManager {
   static final DatabaseManager _instance = DatabaseManager._internal();
@@ -26,14 +25,6 @@ class DatabaseManager {
 
   // Create tables
   Future<void> _onCreate(Database db, int version) async {
-    await db.execute('''
-      CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        email TEXT NOT NULL,
-        password TEXT NOT NULL
-      )
-    ''');
 
     await db.execute('''
       CREATE TABLE IF NOT EXISTS events (
@@ -72,44 +63,6 @@ class DatabaseManager {
     print("Database created and tables initialized.");
   }
 
-  // CRUD for Users
-
-  Future<int> insertUser(Map<String, dynamic> user) async {
-    final db = await database;
-    return await db.insert('users', user);
-  }
-
-  Future<bool> getUser(String email, String password) async {
-    final db = await database;
-    final res = await db.rawQuery(
-      'SELECT * FROM users WHERE email = ? AND password = ?',
-      [email, password],
-    );
-    return res.isNotEmpty ? true : false;
-  }
-
-  Future<List<Map<String, dynamic>>> getUsers() async {
-    final db = await database;
-    return await db.query('users');
-  }
-
-  // Encryption key (used for encrypting and decrypting password)
-  final _key = encrypt.Key.fromLength(32); // 32 bytes key for AES-256 encryption
-  final _iv = encrypt.IV.fromLength(16); // 16 bytes IV for AES encryption
-
-  // Encrypt password before saving to models
-  String encryptPassword(String password) {
-    final encrypter = encrypt.Encrypter(encrypt.AES(_key));
-    final encrypted = encrypter.encrypt(password, iv: _iv);
-    return encrypted.base64; // Return encrypted password as base64
-  }
-
-  // Decrypt password when fetching from models
-  String decryptPassword(String encryptedPassword) {
-    final encrypter = encrypt.Encrypter(encrypt.AES(_key));
-    final decrypted = encrypter.decrypt64(encryptedPassword, iv: _iv);
-    return decrypted;
-  }
 
   // CRUD for Events
 
@@ -169,3 +122,36 @@ class DatabaseManager {
         where: 'user_id = ? AND friend_id = ?', whereArgs: [userId, friendId]);
   }
 }
+
+
+
+// Users
+/*
+  await db.execute('''
+      CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL,
+        password TEXT NOT NULL
+      )
+    ''');
+
+  Future<int> insertUser(Map<String, dynamic> user) async {
+    final db = await database;
+    return await db.insert('users', user);
+  }
+
+  Future<bool> getUser(String email, String password) async {
+    final db = await database;
+    final res = await db.rawQuery(
+      'SELECT * FROM users WHERE email = ? AND password = ?',
+      [email, password],
+    );
+    return res.isNotEmpty ? true : false;
+  }
+
+  Future<List<Map<String, dynamic>>> getUsers() async {
+    final db = await database;
+    return await db.query('users');
+  }
+  */
